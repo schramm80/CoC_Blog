fc73820fc9c744343c3687a6196e94c00f8e9c14
Schramm80 <carlos.schramm80@gmail.com>
2015-04-28 15:10:34 +0200

    Change default devise paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :path_names => { :sign_up => "register",
+                                      :sign_in => "signin",
+                                      :sign_out => "signout" }
   mount Ckeditor::Engine => '/ckeditor'
 
   root to: "articles#index"
