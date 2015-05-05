7dcc0392f1b269ff4d3da54d8408430c7b922803
Schramm80 <carlos.schramm80@gmail.com>
2015-05-05 11:59:50 +0200

    Added model username to users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,29 +3,26 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
 
-  # GET /articles
-  # GET /articles.json
+
   def index
     @articles = Article.all
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
+
   def show
   end
 
-  # GET /articles/new
+
   def new
     @article = Article.new
     @article.user = current_user
   end
 
-  # GET /articles/1/edit
+
   def edit
   end
 
-  # POST /articles
-  # POST /articles.json
+
   require "html_truncator"
   def create
     @article = Article.new(article_params)
@@ -42,8 +39,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
+
   def update
     @article.preview = HTML_Truncator.truncate(@article.body, 300)
 
@@ -58,8 +54,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # DELETE /articles/1
-  # DELETE /articles/1.json
+
   def destroy
     @article.destroy
     respond_to do |format|
@@ -69,12 +64,12 @@ class ArticlesController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
+
   def set_article
     @article = Article.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
+  
   def article_params
     params.require(:article).permit(:title, :body, :published)
   end
