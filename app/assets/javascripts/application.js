480284e72cf0a7f33c2e02b07d481490785a30df
Schramm80 <carlos.schramm80@gmail.com>
2015-05-10 14:44:02 +0200

    Improve views and added brushes to aplication.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+//= require syntaxhighliter/shCore.js
+//= require syntaxhighliter/shBrushBash.js
+//= require syntaxhighliter/shBrushAppleScript.js
+//= require syntaxhighliter/shBrushCpp.js
+//= require syntaxhighliter/shBrushCSharp.js
+//= require syntaxhighliter/shBrushCss.js
+//= require syntaxhighliter/shBrushJScript.js
+//= require syntaxhighliter/shBrushPlain.js
+//= require syntaxhighliter/shBrushRuby.js
+//= require syntaxhighliter/shBrushSass.js
+//= require syntaxhighliter/shBrushSql.js
+//= require syntaxhighliter/shBrushXml.js
